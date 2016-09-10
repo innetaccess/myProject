91aef076f5bf55da7969d1c0cc4dd857eed483ac
innetaccess <innetaccess@gmail.com>
2016-09-10 14:19:36 +0300

    second task

--- level26/lesson15/big01/ConsoleHelper.java
+++ level26/lesson15/big01/ConsoleHelper.java
@@ -1,15 +1,29 @@
 package com.javarush.test.level26.lesson15.big01;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Created by Николай on 20.06.2016.
  */
 public class ConsoleHelper
 {
-    public static void writeMessage(String message){
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+    public static void writeMessage(String message){
+        System.out.println(message);
     }
 
-    static String readString(){
-        return null;
+    public static String readString(){
+        String message = "";
+        try
+        {
+             message = br.readLine();
+        }
+        catch (IOException ignore)
+        {
+        }
+        return message;
     }
 }
---
+++ level26/lesson15/big01/exception/NotEnoughMoneyException.java
@@ -0,0 +1,8 @@
+package com.javarush.test.level26.lesson15.big01.exception;
+
+/**
+ * Created by Администратор on 10.09.2016.
+ */
+public class NotEnoughMoneyException extends Exception
+{
+}
